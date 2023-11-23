d939503285c0687b7a95912036747ebea8952a03
John Pandis <ioanpand@outlook.com>
2023-11-23 19:23:57 +0200

    Delete Javascript folder

--- 2D Controller/JAVASCRIPT/header.js
+++
@@ -1,8 +0,0 @@
-// JAVASCRIPT/header.js
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("/HTML/header.html")
-        .then(response => response.text())
-        .then(headerHtml => {
-            document.body.insertAdjacentHTML('afterbegin', headerHtml);
-        });
-});
